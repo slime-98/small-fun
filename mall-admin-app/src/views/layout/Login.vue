883f33df2c61ae2e071d7832e1b9d524142ab8cf
slime <2773435378@qq.com>
2021-01-19 17:49:25 +0800

    登录
    Signed-off-by: slime <2773435378@qq.com>

---
+++ mall-admin-app/src/views/layout/Login.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="login">
+    <a-form-model
+      ref="loginForm"
+      class="login-form"
+      :model="loginForm"
+      :rules="rules"
+      v-bind="layout"
+    >
+      <a-form-model-item has-feedback label="邮箱" prop="email">
+        <a-input v-model="loginForm.email" placeholder="请输入邮箱" />
+      </a-form-model-item>
+      <a-form-model-item has-feedback label="密码" prop="password">
+        <a-input
+          v-model="loginForm.password"
+          type="password"
+          autocomplete="off"
+		  placeholder="请输入密码"
+        />
+      </a-form-model-item>
+      <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+        <a-button type="primary" @click="submitForm('loginForm')">
+          提交
+        </a-button>
+        <a-button style="margin-left: 10px" @click="resetForm('loginForm')">
+          重置
+        </a-button>
+      </a-form-model-item>
+    </a-form-model>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    let emailReg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+    let checkEmail = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("请输入邮箱"));
+      }
+      if (emailReg.test(value)) {
+        return callback();
+      } else {
+        return callback(new Error("邮箱格式不正确"));
+      }
+    };
+    let validatePass = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error("请输入密码"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      loginForm: {
+        password: "",
+        email: "",
+      },
+      rules: {
+        password: [{ validator: validatePass, trigger: "change" }],
+        email: [{ validator: checkEmail, trigger: "change" }],
+      },
+      layout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 14 },
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          console.log(this.loginForm);
+          return alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
+</script>
+
+
+<style lang="less" scoped>
+@import url("~@/assets/css/login.less");
+</style>
